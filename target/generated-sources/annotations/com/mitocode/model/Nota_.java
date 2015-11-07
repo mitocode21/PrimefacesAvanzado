8967ae8c52fd01b6947ee731cff67752d5699422
mitocode21 <mitocode21@gmail.com>
2015-11-07 10:27:27 -0500

    Hasta tuto 21

--- target/generated-sources/annotations/com/mitocode/model/Nota_.java
+++ target/generated-sources/annotations/com/mitocode/model/Nota_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-10-04T19:17:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-10-29T22:12:02")
 @StaticMetamodel(Nota.class)
 public class Nota_ { 
 
